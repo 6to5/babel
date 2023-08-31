67771109c93896ceaccecdcc7c5896cbc8eb789e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-08-31 12:15:53 +0200

    Use relative filename

--- packages/babel-core/src/transformation/normalize-opts.ts
+++ packages/babel-core/src/transformation/normalize-opts.ts
@@ -35,7 +35,10 @@ export default function normalizeOptions(config: ResolvedConfig): {} {
       // TODO: @babel/parser uses sourceFilename, while @babel/generator and
       // @babel/core use sourceFileName. Eventualy align them.
       // https://github.com/babel/babel/pull/13518
-      sourceFilename: filename,
+      // We use the relative filename here so that source maps do not include
+      // absolute paths, and are thus portable.
+      sourceFilename:
+        filenameRelative === "unknown" ? undefined : filenameRelative,
       plugins: [],
       ...opts.parserOpts,
     },
